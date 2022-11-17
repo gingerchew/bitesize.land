2e43e5622d26e7e4c9e96172c636d1626d02766c
Frankie <26461046+frankie-tech@users.noreply.github.com>
2022-11-17 14:23:29 -0500

    Adds Toggle for gzip & brotli

--- islands/Controls.tsx
+++ islands/Controls.tsx
@@ -1,83 +1,82 @@
 import Control from "#/islands/Control.tsx";
+import Toggle from "#/components/Toggle.tsx";
 import { useContext } from "preact/hooks";
 import { JSX } from "https://esm.sh/v94/preact@10.11.0/jsx-runtime/src/index.d.ts";
 import { ControlContext } from "#/islands/EditorArea.tsx";
 
 const SummaryWrapper = (props: JSX.HTMLAttributes<HTMLDetailsElement>) => {
-    return <details>
+  return (
+    <>
+      <details>
         <summary>{props.title}</summary>
         <div className="content">
-            {props.children}
+          {props.children}
         </div>
-    </details>
-}
-
+      </details>
+    </>
+  );
+};
 
 export default function Controls() {
+  const { currentControls, setControl } = useContext(ControlContext);
 
-    const { currentControls, setControl } = useContext(ControlContext);
-
-    return <>
-    <Control
-        id="useGzip"
-        label="Enable GZIP"
-        type="checkbox"
-        checked={currentControls?.isGzipChecked}
-        onChange={({ target }) => {
-            const el = target as HTMLInputElement;
-            
-            setControl({
-                ...currentControls,
-                isGzipChecked: el?.checked
-            })
-
-        }}
-    />
-    <Control
-        id="useBrotli"
-        label="Enable Brotli"
-        type="checkbox"
-        checked={currentControls.isBrotliChecked}
-        onChange={({ target }) => {
-            const el = target as HTMLInputElement;
-            setControl({
-                ...currentControls,
-                isBrotliChecked: el?.checked
-            })
-        }}
-    />
-    <Control
-        id="includeWhiteSpace"
-        label="Include White Space?"
-        type="checkbox"
-        checked={!currentControls.isWhiteSpaceIgnored}
-        onChange={({ target }) => {
-            const el = target as HTMLInputElement;
-            setControl({
-                ...currentControls,
-                isWhiteSpaceIgnored: !el?.checked
-            })
-        }}
-    />
-    <SummaryWrapper title="Advanced">
+  return (
+    <>
+        <div class="controls-compression">
+            <Toggle 
+                for="useGzip"
+                label="GZIP"
+                onChange={({ target }) => setControl({
+                    ...currentControls,
+                    isGzipChecked: (target as HTMLInputElement)?.checked
+                })}
+            />
+            <Toggle 
+                for="useBrotli"
+                label="Brotli"
+                onChange={({ target }) => setControl({
+                    ...currentControls,
+                    isBrotliChecked: (target as HTMLInputElement)?.checked
+                })}
+            />
+        </div>
         <Control
+          id="includeWhiteSpace"
+          label="Include White Space?"
+          type="checkbox"
+          checked={!currentControls.isWhiteSpaceIgnored}
+          onChange={({ target }) => {
+            const el = target as HTMLInputElement;
+            setControl({
+              ...currentControls,
+              isWhiteSpaceIgnored: !el?.checked,
+            });
+          }}
+        />
+        <SummaryWrapper title="Advanced">
+          <Control
             id="gzipLevel"
             label="GZIP Level"
             type="select"
             onChange={({ target }) => {
-                const el = target as HTMLSelectElement;
-                setControl({
-                    ...currentControls,
-                    gzipLevel: +el.value
-                })
+              const el = target as HTMLSelectElement;
+              setControl({
+                ...currentControls,
+                gzipLevel: +el.value,
+              });
             }}
-        >
-            <option selected={currentControls.gzipLevel === 1} value="1">Low</option>
-            <option selected={currentControls.gzipLevel === 6} value="6">
-                Default
+          >
+            <option selected={currentControls.gzipLevel === 1} value="1">
+              Low
             </option>
-            <option selected={currentControls.gzipLevel === 9} value="9">High</option>
-        </Control>
-    </SummaryWrapper>
-  </>
+            <option selected={currentControls.gzipLevel === 6} value="6">
+              Default
+            </option>
+            <option selected={currentControls.gzipLevel === 9} value="9">
+              High
+            </option>
+          </Control>
+        </SummaryWrapper>
+    </>
+  );
 }

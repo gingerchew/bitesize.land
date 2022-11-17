85e8d26e3d49cb305a1ab78dbfd93832eb345a39
Frankie <26461046+frankie-tech@users.noreply.github.com>
2022-11-17 18:40:16 -0500

    consolidates controls styles, removes old imports

--- islands/Controls.tsx
+++ islands/Controls.tsx
@@ -1,12 +1,81 @@
-import Control from "#/islands/Control.tsx";
 import Toggle from "#/components/Toggle.tsx";
 import { useContext } from "preact/hooks";
-import { JSX } from "https://esm.sh/v94/preact@10.11.0/jsx-runtime/src/index.d.ts";
 import { ControlContext } from "#/islands/EditorArea.tsx";
+
+export const controlStyles = `
+.controls {
+  padding: 1vmin;
+  padding-left: 2vmin;
+  width: 100%;
+  grid-column: 1;
+  grid-row: 2;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  align-items: center;
+}
+.controls > * {
+  grid-row: 1;
+}
+.controls-compression {
+  height: auto;
+  transition: max-height 0.25s ease-out;
+  justify-content: start;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  grid-row: 2;
+  grid-column: 1 / -1;
+}
+.controls svg {
+  --stroke-color: transparent;
+  overflow: visible;
+}
+.controls[data-pane="true"] svg {
+  transition: color 0.25s ease-out;
+  color: var(--paper);
+  --stroke-color: var(--ink);
+}
+
+.controls path {
+  stroke-width: 2rem;
+  stroke: var(--stroke-color);
+  transition: stroke-color 0.25s ease-out;
+}
+@media (min-width: 768px) {
+  .controls {
+    grid-template-columns: minmax(min-content, max-content) 2fr;
+    grid-auto-rows: auto;
+    gap: 1rem;
+  }
+  .controls-compression {
+    overflow: hidden;
+    max-height: 4ch;
+  }
+  .controls:not([data-pane="true"]) .controls-compression{
+    max-height: 0px;
+  }
+  .controls .controls-compression {
+    grid-column: 2;
+    grid-row: 1;
+  }
+}
+.control {
+  display: flex;
+  gap: 0.25ch;
+  justify-content: center;
+}
+
+@media (min-width: 768px) {
+  .control {
+    justify-content: start;
+    text-align: left;
+  }
+}`
+
 
 export default function Controls() {
   const { currentControls, setControl } = useContext(ControlContext);
-
+  
   return (
       <div class="controls-compression">
           <Toggle 
@@ -30,10 +99,10 @@ export default function Controls() {
         <Toggle
           for="includeWhiteSpace"
           label="Include White Space"
-          checked={!currentControls.isWhiteSpaceIgnored}
+          checked={currentControls.isWhiteSpaceIncluded}
           onChange={({ target }) => setControl({
             ...currentControls,
-            isWhiteSpaceIgnored: (target as HTMLInputElement)?.checked
+            isWhiteSpaceIncluded: (target as HTMLInputElement)?.checked
           })}
         />
       </div>

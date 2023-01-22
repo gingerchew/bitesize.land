ba4f9a6c030602376df5ecda5df944c842ebc44a
ginger <26461046+gingerchew@users.noreply.github.com>
2023-01-21 21:21:07 -0500

    Removes localStorage from useState in mode toggle

--- islands/DarkmodeToggle.tsx
+++ islands/DarkmodeToggle.tsx
@@ -36,7 +36,7 @@ export default function DarkmodeToggle() {
     */
     // const curr = getInitialMode();
     // use curr to set state
-    const [mode, setMode] = useState(localStorage.dm || matchMedia('prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+    const [mode, setMode] = useState(window.localStorage?.dm || matchMedia('prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 
     const onClick = () => {
         const c = mode;
